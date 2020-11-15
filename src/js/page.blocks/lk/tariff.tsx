c6108ec1e4c8b4376c57ba18ecedb4106e8312b1
sowingsadness <swsdss@live.com>
2020-11-16 02:03:04 +0300

    Убрал из тарифа раздел дополнительные услуги, если их нет

--- src/js/page.blocks/lk/tariff.tsx
+++ src/js/page.blocks/lk/tariff.tsx
@@ -21,6 +21,21 @@ interface IProps {
 }
 
 export default function MyTariff(props: Readonly<IProps>): React.ReactElement {
+
+    const AddServices = props.data.addServices.length === 0 ? '' : <React.Fragment>
+        <p className="lk__little-text lk__little-text_bold-mini lk__little-text_bold">Дополнительные услуги</p>
+        <div className="lk__row lk__row_padding lk__row_first">
+            <p className=" lk__little-text  lk__little-text_bold lk__little-text_bold-mini lk__little-text_no-margin">Услуга:</p>
+            <p className="lk__little-text lk__little-text_bold lk__little-text_bold-mini lk__little-text_no-margin">Доступно до:</p>
+        </div>
+        { props.data.addServices.map((item, index) => {
+            return <div className="lk__row lk__row_blue lk__row_padding" key={ index }>
+                <p className="lk__little-text_no-margin lk__little-text">{ item.service }</p>
+                <p className="lk__little-text_no-margin lk__little-text">{ item.paidUntil }</p>
+            </div>
+        }) }
+    </React.Fragment>;
+
     return <div className="profile-settings profile-settings_lk profile-settings_rates">
         <div className="profile-settings__title-wrapper">
             <h2 className="profile-settings__title">Мой тариф</h2>
@@ -51,17 +66,7 @@ export default function MyTariff(props: Readonly<IProps>): React.ReactElement {
                     { props.data.paidUntil }
                 </p>
             </div>
-            <p className="lk__little-text lk__little-text_bold-mini lk__little-text_bold">Дополнительные услуги</p>
-            <div className="lk__row lk__row_padding lk__row_first">
-                <p className=" lk__little-text  lk__little-text_bold lk__little-text_bold-mini lk__little-text_no-margin">Услуга:</p>
-                <p className="lk__little-text lk__little-text_bold lk__little-text_bold-mini lk__little-text_no-margin">Доступно до:</p>
-            </div>
-            { props.data.addServices.map((item, index) => {
-                return <div className="lk__row lk__row_blue lk__row_padding" key={ index }>
-                    <p className="lk__little-text_no-margin lk__little-text">{ item.service }</p>
-                    <p className="lk__little-text_no-margin lk__little-text">{ item.paidUntil }</p>
-                </div>
-            }) }
+            { AddServices }
         </div>
     </div>;
 }
