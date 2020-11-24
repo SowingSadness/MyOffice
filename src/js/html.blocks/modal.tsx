c5b4806980ae7936e572ae874d569d1e9224463b
sowingsadness <swsdss@live.com>
2020-11-24 05:43:34 +0300

    +

--- src/js/html.blocks/modal.tsx
+++ src/js/html.blocks/modal.tsx
@@ -69,7 +69,7 @@ export default class ModalWindow<T> extends React.Component<IProps<T & IModalPro
         }
         this.setState({ visible: false });
 
-        this.props.signal?.props?.onClose.apply(undefined, arguments);
+        this.props.signal?.props?.onClose?.apply(undefined, arguments);
     };
 
     render() {
